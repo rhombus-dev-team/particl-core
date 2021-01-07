a2b947d91f145f2131180ddac329fbebed7f172b
xploited <xploited.ca@gmail.com>
2021-01-07 18:59:52 -0400

    update gitian descriptors and add dns seed

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -356,7 +356,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        //vSeeds.emplace_back("mainnet.rhom.com");
+        vSeeds.emplace_back("mainnet.rhom.com");
 
 
         base58Prefixes[PUBKEY_ADDRESS]     = {0x3c}; // R
